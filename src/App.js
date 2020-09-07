8127103ec6c4bd1076641755f3f283022be64363
Kendra <odrunia.km@gmail.com>
2020-09-07 11:34:35 -0700

    add finding tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+function getUser() {
+  return Promise.resolve({ id: "1", name: "Robin" });
+}
+
 function App() {
+  const title = "Learn React Testing";
+  const [search, setSearch] = React.useState("");
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, []);
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
+    <div>
+      <h1>{title}</h1>
+      {user ? <p>Signed in as {user.name}</p> : null}
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+
+      <p>Searches for {search ? search : "..."}</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+function Search({ value, onChange, children }) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" type="text" value={value} onChange={onChange} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,71 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 // test takes in two args. The descrip of the test is runs and an anonymous func
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders testing first lesson", () => {
+    render(<App />);
+
+    // screen.debug(); renders the html in our terminal
+
+    // a string argument is used for the exact match,
+    // a regular expression can be used for a partial match which
+    // is often more convenient:
+    // fails
+    // expect(screen.getByText("Search")).toBeInTheDocument();
+
+    // succeeds - looks for exact match
+    expect(screen.getByText("Search:")).toBeInTheDocument();
+
+    // succeeds -
+    expect(screen.getByText(/Search:/)).toBeInTheDocument();
+  });
+  test("react testing second lesson", () => {
+    render(<App />);
+
+    // screen.getByRole(""); react provides you avail roles, when there's no input
+
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+  test("react testing third lesson", () => {
+    render(<App />);
+    // screen.debug();
+
+    // fails
+    // expect(screen.getByText(/Searches for JavaScript/)).toBeNull();
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+  });
+  test("react testing fourth lesson, using findByText", async () => {
+    render(<App />);
+    // null at first render
+    // screen.debug();
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+    // Signed in as after fetching user
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+    // screen.debug();
+  });
+  test("react testing interaction", () => {
+    render(<App />);
+
+    screen.debug();
+
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "JavaScript" },
+    });
+
+    screen.debug();
+  });
 });
+
+// What's the difference between getBy vs queryBy?
+// getBy returns an element or an error. It's a convenient side-effect of
+// getBy that it returns an error,
+// However, this makes it difficult to check for elements which shouldn't
+// be there:
+// queryBy:
+//  In order to assert elements which aren't there, we can exchange
+// getBy with queryBy:
+// FindBy:
+// The findBy search variant is used for asynchronous elements which will be
+// there eventually. For a suitable scenario
